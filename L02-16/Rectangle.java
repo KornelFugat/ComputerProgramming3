ecf5d72c39e76bfbfa8f5383bbb713104508accc
KornelFugat <79914876+KornelFugat@users.noreply.github.com>
2021-11-14 18:28:03 +0100

    Delete L02-16 directory

--- L02-16/Rectangle.java
+++
@@ -1,9 +0,0 @@
-
-public class Rectangle{
-    int height;
-    int length;
-    
-    public void display(){
-        System.out.println("dimensions: " + height + "x" + length +"\nperimeters: " + 2*length+2*height + "\nsurface: " + height*length);
-    }
-}
